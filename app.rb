22e19271dc6d3c43093359ffb989f2d312416f2d
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-21 18:07:45 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require 'sinatra'
 
 class Application < Sinatra::Base
+  
   get '/' do
     erb :index
   end
+  
+  post '/greet' do
+    erb :greet
+  end
+  
 end
 
 method = Application.method(:get)
