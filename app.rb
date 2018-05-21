171011682bf9209ea7d3715cd89ad0943d11b0b8
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-21 18:07:59 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
+require 'sinatra'
+
 class Application < Sinatra::Base
-  # Write your code here!
+
+  get '/' do
+    erb :index
+  end
+
+  post '/greet' do
+    erb :greet
+  end
 
 end
+
+method = Application.method(:get)
+location = method.source_location.inspect
+puts "Source location is: #{location}"
